a60c66c7af971d0c9337bdbe727d60c04a1bfedd
krimokt <krimokt@gmail.com>
2025-04-04 18:17:12 +0800

    Fix: Convert shipping components to Client Components

--- src/components/shipping/ShippingRouteMap.tsx
+++ src/components/shipping/ShippingRouteMap.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import { worldMill } from "@react-jvectormap/world";
 import dynamic from "next/dynamic";
--- src/components/shipping/ShippingTracking.tsx
+++ src/components/shipping/ShippingTracking.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import ShippingRouteMap, { ShippingRoute } from "./ShippingRouteMap";
 import ShippingTrackingTable, { ShippingTrackingItem } from "./ShippingTrackingTable";
