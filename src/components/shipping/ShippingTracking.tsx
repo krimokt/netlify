ea46ae065ae7906d08150e807502fda975293a14
krimokt <krimokt@gmail.com>
2025-04-04 18:20:17 +0800

    Remove map from shipping tracking component

--- src/components/shipping/ShippingTracking.tsx
+++ src/components/shipping/ShippingTracking.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React from "react";
-import ShippingRouteMap, { ShippingRoute } from "./ShippingRouteMap";
 import ShippingTrackingTable, { ShippingTrackingItem } from "./ShippingTrackingTable";
 import Button from "../ui/button/Button";
 
@@ -63,64 +62,6 @@ const sampleTrackingData: ShippingTrackingItem[] = [
   }
 ];
 
-// Convert tracking data to map routes
-const mapRoutes: ShippingRoute[] = [
-  {
-    id: "route-1",
-    origin: {
-      country: "China",
-      name: "Shenzhen",
-      latLng: [22.5431, 114.0579] // Shenzhen coordinates
-    },
-    current: {
-      country: "Singapore",
-      name: "Singapore Port",
-      latLng: [1.3521, 103.8198] // Singapore coordinates
-    },
-    destination: {
-      country: "Morocco",
-      name: "Casablanca",
-      latLng: [33.5731, -7.5898] // Casablanca coordinates
-    }
-  },
-  {
-    id: "route-2",
-    origin: {
-      country: "China",
-      name: "Shanghai",
-      latLng: [31.2304, 121.4737] // Shanghai coordinates
-    },
-    current: {
-      country: "Egypt",
-      name: "Port Said",
-      latLng: [31.2565, 32.2841] // Port Said coordinates
-    },
-    destination: {
-      country: "Morocco",
-      name: "Casablanca",
-      latLng: [33.5731, -7.5898] // Casablanca coordinates
-    }
-  },
-  {
-    id: "route-3",
-    origin: {
-      country: "China",
-      name: "Guangzhou",
-      latLng: [23.1291, 113.2644] // Guangzhou coordinates
-    },
-    current: {
-      country: "Morocco",
-      name: "Casablanca",
-      latLng: [33.5731, -7.5898] // Casablanca coordinates
-    },
-    destination: {
-      country: "Morocco",
-      name: "Casablanca",
-      latLng: [33.5731, -7.5898] // Casablanca coordinates
-    }
-  }
-];
-
 const ShippingTracking: React.FC = () => {
   return (
     <div className="rounded-xl border border-gray-200 bg-white dark:border-white/[0.05] dark:bg-white/[0.03]">
@@ -132,13 +73,6 @@ const ShippingTracking: React.FC = () => {
           <Button variant="primary" size="sm" className="bg-[#1E88E5] hover:bg-[#0D47A1]">
             View All Shipments
           </Button>
-        </div>
-      </div>
-
-      {/* Map Section */}
-      <div className="px-5 pb-6">
-        <div className="w-full h-80 overflow-hidden border border-gray-200 rounded-xl dark:border-white/[0.05]">
-          <ShippingRouteMap routes={mapRoutes} />
         </div>
       </div>
 
